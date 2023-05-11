0baf3626f10c000a687c0f41b8e12e07ff2d3f9f
ra230797 <m230797@dac.unicamp.br>
2023-05-11 01:38:32 -0300

    fazendo menuOperacoes

--- Lab04/src/AppMain.java
+++ Lab04/src/AppMain.java
@@ -1,9 +1,108 @@
 import java.time.LocalDate;
 import java.util.ArrayList;
-
-import Validacao.validacaoS;
+import java.util.Scanner;
 
 public class AppMain {
+    //funcao de checar caso
+    public static void qualOperacaoPrincipal(MenuOperacoes operacao, Seguradora seg){
+        switch(operacao){
+            //cadastrar (1)
+            case CADASTRAR:
+                int comandoC = 0;
+                Scanner scanC = new Scanner(System.in);
+                //Mostra as opcoes
+                System.out.println("1.1 - Cadastrar Cliente\n1.2 - Cadastrar Veiculo\n1.3 - Cadastrar Seguradora\n1.4 - Voltar\n");
+                //Enquanto nao voltar
+                while(comandoC != 4){
+                    //pergunta qual opcao o usuario quer
+                    System.out.println("Qual acao 1.x voce deseja tomar? x = ?\n");
+                    comandoC = scanC.nextInt();
+                    if(comandoC == 1){
+                        //Cadastrar cliente pf ou pj
+                    }
+                    else if(comandoC == 2){
+                        //Cadastrar veiculo
+                    }
+                    else if(comandoC == 3){
+                        //Cadastrar seguradora
+                    }
+                }
+                break;
+            //listar (2)
+            case LISTAR:
+                System.out.println("2.1 - Listar Cliente (PF/PJ) por Seguradora\n2.2 - Listar Sinistros por Seguradora\n2.3 - Listar Sinistro por Cliente\n2.4 - Listar Veiculo por Cliente\n2.5 - Listar Veiculo por Seguradora\n2.6 - Voltar\n");
+                break;
+            //excluir (3)
+            case EXCLUIR:
+                System.out.println("3.1 - Excluir Cliente\n3.2 - Excluir Veiculo\n3.3 - Excluir Sinistro\n3.4 - Voltar");
+                break;
+            //gerar sinistro (4)
+            case GERAR_SINISTRO:
+                Scanner scan = new Scanner(System.in);
+                //Recebendo o veiculo do sinistro
+                System.out.println("Qual a placa do veiculo envolvido no sinistro?\n");
+                String vSin = scan.next();
+                //Recebendo o cliente do sinistro 
+                System.out.println("Quem é o dono do veiculo?\n");
+                String cSin = scan.next();
+                //Recebendo o endereco do sinistro
+                System.out.println("Onde ocorreu o sinistro?\n");
+                String endSin = scan.next();
+                //Procurando objeto cliente que correponde cSin
+                for(Cliente c : seg.getListaClientes()){
+                    if(c.getNome() == cSin){
+                        //Procurando o objeto veiculo que corresponde vSin, o qual cSin é dono
+                        for(Veiculo v : c.getListaVeiculos()){
+                            if(v.getPlaca() == vSin){
+                                //gerar sinistro com esse cliente e esse veiculo
+                                seg.gerarSinistro(v, c, endSin);
+                            }
+                        }
+                    }
+                }
+                scan.close();
+                
+            //transferir seguro (5)
+            case TRANSFERIR_SEGURO:
+                //recebendo o que vai transferir
+                Scanner scan1 = new Scanner(System.in);
+                System.out.println("Digite o cliente que irá transferir:\n");
+                String cliente_transf = scan1.next();
+                //recebendo o que vai receber
+                System.out.println("Digite o cliente que irá receber:\n");
+                String cliente_recebe = scan1.next();
+                //fechando scanner
+                scan1.close();
+                //Procurando o cliente_transf
+                Cliente ct;
+                for(Cliente c : seg.getListaClientes()){
+                    if(c.getNome() == cliente_transf){
+                        ct = c;
+                    }
+                }
+                //Procurando o cliente que vai receber
+                for(Cliente c : seg.getListaClientes()){
+                    if(c.getNome() == cliente_recebe){
+                        //Transferindo os veiculos para ele
+                        for(Veiculo v : ct.getListaVeiculos()){
+                            c.getListaVeiculos().add(v);
+                            //Esvaziando a lista do cliente_transf
+                            ct.getListaVeiculos().remove(v);
+                        }
+                    }
+                }
+                break;
+            
+            //calcular receita seguradora (6)
+            case CALCULAR_RECEITA_SEGURADORA:
+                System.out.println("Receita: " + seg.calcularReceita() + "\n");
+                break;
+            //sair (0)
+            case SAIR:
+                System.exit(0);
+        }
+    }
+
     public static void appMain(String args[]){
         //Instanciando dois veiculos
         Veiculo v1 = new Veiculo("PLACA1", "Honda", "Civic", 2022);
@@ -68,6 +167,23 @@ public class AppMain {
         //Printando a receita da seguradora
         System.out.println(receita_seg);
 
-        //FUNCAO DE MENU DE OPERACOES
+        //FUNCAO DE MENU DE OPERACOES//
+        System.out.println("1 - Cadastros\n2 - Listar\n3 - Excluir\n4 - Gerar Sinistro\n5 - Transferir Seguro\n6 - Calcular Receita Seguradora\n0 - Sair\n");
+        int comando;
+        Scanner entrada = new Scanner(System.in);
+        //leio o comando
+        comando = entrada.nextInt();
+        while(true){
+            //está no menu principal
+            if(comando == -1){
+                System.out.println("1 - Cadastros\n2 - Listar\n3 - Excluir\n4 - Gerar Sinistro\n5 - Transferir Seguro\n6 - Calcular Receita Seguradora\n0 - Sair\n");
+                //acao que o usuario quer tomar
+                comando = entrada.nextInt();
+            }
+            //Procuro no MenuOperacoes a operacao que corresponde o valor do comando
+            MenuOperacoes operacao = MenuOperacoes.getOperacao(comando);
+            qualOperacaoPrincipal(operacao, seg);
+        }
     }
 }
+
--- Lab04/src/MenuOperacoes.java
+++ Lab04/src/MenuOperacoes.java
@@ -1,6 +1,10 @@
 public enum MenuOperacoes{
     CADASTRAR(1),
-    EXCLUIR(2),
+    LISTAR(2),
+    EXCLUIR(3),
+    GERAR_SINISTRO(4),
+    TRANSFERIR_SEGURO(5),
+    CALCULAR_RECEITA_SEGURADORA(6),
     SAIR(0);
 
     public final int operacao;
@@ -9,7 +13,17 @@ public enum MenuOperacoes{
         this.operacao = operacao;
     }
 
-    public int getOperacao(){
+    public int getOperacaoValor(){
         return this.operacao;
     }
+
+    public static MenuOperacoes getOperacao(int valor){
+        for(MenuOperacoes operacao : MenuOperacoes.values()){
+            if(valor == operacao.getOperacaoValor()){
+                return operacao;
+            }
+        }
+        return null;
+    }
+
 }
